be5395f4ef059dac8e0c87b79800db0b127aeddf
ymori <ymori@student.42tokyo.jp>
2022-07-29 17:55:02 +0900

    finish CPP01/ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -1,3 +1,21 @@
 //
 // Created by yuumo on 2022/07/29.
 //
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*strPTR = &str;
+	std::string &strREF = str;
+
+	std::cout << "str                     : " << str << std::endl;
+	std::cout << "str address (&str)      : " << &str << std::endl;
+
+	std::cout << "strPTR                  : " << strPTR << std::endl;
+	std::cout << "strPTR content (*strPTR): " << *strPTR << std::endl;
+
+	std::cout << "strREF                  : " << strREF << std::endl;
+	std::cout << "strREF address (&strREF): " << &strREF << std::endl;
+}
