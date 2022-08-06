4b0d89b9f6fd65e4209774b917c72e27e7213d1a
ymori <ymori@student.42tokyo.jp>
2022-08-07 00:07:59 +0900

    CPP06/ex00: add: printChar and convertChar

---
+++ CPP06/ex00/ScalarConversion.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yuumo on 2022/08/06.
+//
+
+#include "ScalarConversion.hpp"
---
+++ CPP06/ex00/ScalarConversion.hpp
@@ -0,0 +1,16 @@
+//
+// Created by yuumo on 2022/08/06.
+//
+
+#ifndef EX00_SCALARCONVERSION_HPP
+#define EX00_SCALARCONVERSION_HPP
+
+
+
+class ScalarConversion {
+
+};
+
+
+
+#endif //EX00_SCALARCONVERSION_HPP
