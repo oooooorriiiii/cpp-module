f8165da6c49beda11a202b8b4da60776b6060cfc
ymori <ymori@student.42tokyo.jp>
2022-08-12 00:41:59 +0900

    CPP08/ex01

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -33,6 +33,28 @@ int	main() {
 		}
 	}
 	{
+		std::cout << "[TEST]: subject main: minus" << std::endl;
+		try
+		{
+			Span sp = Span(5);
+
+			sp.addNumber(6);
+			sp.addNumber(-3);
+			sp.addNumber(17);
+			sp.addNumber(-9);
+			sp.addNumber(11);
+
+//		sp.setPrint();
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+
+		}
+		catch (std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	{
 		std::cout << "[TEST]: duplicate elements: all" << std::endl;
 		try
 		{
