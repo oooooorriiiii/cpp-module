373d9981c3d5584980e39077d909e4fd9cd5d773
ymori <yuu.mori06@gmail.com>
2022-09-05 18:12:01 +0900

    fix: CPP01/ex04 main()

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <fstream>
 
-void	replace(std::string filename, std::string s1, std::string s2) {
+void	replace(const std::string& filename, const std::string& s1, const std::string& s2) {
 	std::ifstream ifd(filename.c_str());
 	if (!ifd) {
 		std::cerr << "Error" << std::endl;
@@ -35,8 +35,14 @@ void	replace(std::string filename, std::string s1, std::string s2) {
 }
 
 int main(int argc, char **argv) {
-	if (argc == 4 && argv[1] && argv[2] && argv[3])
+	if (argc == 4)
+	{
+		if (static_cast<std::string>(argv[3]).find(static_cast<std::string>(argv[2])) != std::string::npos) {
+			std::cout << "Error: The original string is contained in the string to be replaced" << std::endl;
+			return 0;
+		}
 		replace(argv[1], argv[2], argv[3]);
+	}
 	else
 		std::cerr << "Argument error" << std::endl;
 	return 0;
